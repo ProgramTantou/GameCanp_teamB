3bfdcff346e289e4ebe74e7c4f222795dd1b1ed3
Hiziman0218 <hinata.m0218@icloud.com>
2024-03-06 12:59:32 +0900

    field karituika

---
+++ Project/GameProject/Game/Field.cpp
@@ -0,0 +1,46 @@
+#include "Field.h"
+
+//コンストラクタ
+Field::Field()
+	:m_pos()
+	,mp_image(nullptr)
+{
+	//フィールドの画像を読み込み
+	mp_image = CImage::CreateImage("Field.png");
+	mp_image->SetCenter(960.0f,540.0f);
+	m_ground_y = 540;
+}
+
+//デストラクタ
+Field::~Field() {
+	//画像データを削除
+	delete mp_image;
+}
+
+//フィールドの座標を取得
+const CVector2D& Field::GetPos() const {
+	
+}
+
+//フィールドの座標を設定
+void Field::SetPos(const CVector2D& pos) {
+	m_pos = pos;
+}
+
+//フィールドの高さを取得
+const float Field::GetGroundY(){
+	return m_ground_y;
+}
+
+//更新処理
+void Field::Update() {
+	//イメージに座標を設定
+	mp_image->SetPos(m_pos);
+}
+
+//描画処理
+void Field::Draw() {
+	float sc;
+	mp_image->Draw();
+	//sc = m_scroll.x
+}
--- Project/GameProject/Game/Field.h
+++ Project/GameProject/Game/Field.h
@@ -1,8 +1,39 @@
 #pragma once
 class Field {
 public:
+	//コンストラクタ
 	Field();
+	//デストラクタ
 	~Field();
+
+	/// <summary>
+	/// フィールドの座標を取得
+	/// </summary>
+	/// <returns>フィールドの座標</returns>
+	const CVector2D& GetPos() const;
+
+	/// <summary>
+	/// フィールドの座標を設定
+	/// </summary>
+	/// <param name="pos"></param>
+	void SetPos(const CVector2D& pos);
+
+	//更新処理
+	void Update();
+	//描画処理
+	void Draw();
+
+	/// <summary>
+	/// フィールドの高さを取得
+	/// </summary>
+	/// <returns>フィールドの高さ</returns>
+	const float GetGroundY();
+
 private:
+	CVector2D m_pos;  //フィールドの座標
+	CImage* mp_image; //フィールドの画像
+	float m_ground_y; //フィールドの当たり判定
+
+protected:
 
 };

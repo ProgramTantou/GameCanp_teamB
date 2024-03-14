b1c3f555767a179a65e1958451cbd84a75ff5ebe
shark954 <23051905@d.anabuki-college.net>
2024-03-14 15:49:38 +0900

    s

--- Project/GameProject/Game/Fish.cpp
+++ Project/GameProject/Game/Fish.cpp
@@ -14,13 +14,13 @@ Fish::Fish(const CVector3D& pos,int fish) :ObjectBase(eType_Fish)
 	switch (m_fish) 
 	{
 	case eFish_1:
-		//m_img.Load("",fish_anim_data , , );
+		//m_img.Load("Image/ika.png",fish_anim_data , 766,766 );
 		break;
 	case eFish_2:
-		//m_img.Load("",fish_ainm_data, , );
+		//m_img.Load("Image/warasubo.png",fish_ainm_data,731 ,731 );
 		break;
 	case eFish_3:
-		//m_img.Load("",fish_ainm_data, , );
+		//m_img.Load("Image/nijimasu.png",fish_ainm_data,800 ,513 );
 		break;
 	}
 	m_pos = pos;
@@ -30,14 +30,20 @@ Fish::Fish(const CVector3D& pos,int fish) :ObjectBase(eType_Fish)
 	int move_speed = 3;
 	if (m_fish == eFish_1) 
 	{
+		m_img.SetSize(766/2, 766/2);
+		m_img.SetCenter(766 / 4, 766 / 4);
 		m_pos.x += (move_speed + 3);
 	}
 	else if (m_fish == eFish_2) 
 	{
+		m_img.SetSize(731 / 2, 731 / 2);
+		m_img.SetCenter(731 / 4, 731 / 4);
 		m_pos.x += (move_speed + 2);
 	}
 	else if(m_fish==eFish_3)
 	{
+		m_img.SetSize(800 / 2, 513/ 2);
+		m_img.SetCenter(800 / 4, 513 / 4);
 		m_pos.x += (move_speed + 1);
 	}
 

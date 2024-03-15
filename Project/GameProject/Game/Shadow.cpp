6bfeccc46dd6abc519e7b20a07b970162bb0cfc6
ProgramTantou <23051907@d.anabuki-college.net>
2024-03-15 17:31:45 +0900

    Shadow.Update

--- Project/GameProject/Game/Shadow.cpp
+++ Project/GameProject/Game/Shadow.cpp
@@ -23,13 +23,15 @@ void Shadow::Render() {
 		if (obj->m_type == eType_Fish)
 		{
 		m_img.SetSize(50, 50);
+		m_img.SetCenter(25, 25);
 		m_img.SetPos(obj->GetScreenPos(CVector3D(obj->m_pos.x, 800, obj->m_pos.z)));
 		m_img.Draw();
 		}
 		else
 		{
 			m_img.SetSize(200, 200);
-			m_img.SetPos(obj->GetScreenPos(CVector3D(obj->m_pos.x, 800, obj->m_pos.z-100)));
+			m_img.SetCenter(100, 100);
+			m_img.SetPos(obj->GetScreenPos(CVector3D(obj->m_pos.x, 750, obj->m_pos.z)));
 			m_img.Draw();
 		}
 	}

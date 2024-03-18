55076fd0a1eb88143061a3ac4beb5ae49ed2f9e2
shark954 <23051905@d.anabuki-college.net>
2024-03-18 15:52:22 +0900

    d

--- Project/GameProject/Game/AnimData.cpp
+++ Project/GameProject/Game/AnimData.cpp
@@ -5,50 +5,57 @@ int frame = 10;
 
 static TexAnim playerIdle[] = {
 	{0,frame},
-	{8,frame},
-	{16,frame},
-	{24,frame},
-	{32,frame},
+	{1,frame},
+	{2,frame},
+	{3,frame},
+	{4,frame},
 };
 static TexAnim playerRun[] = {
-	{1,frame},
+	{8,frame},
 	{9,frame},
-	{17,frame},
-	{25,frame},
+	{10,frame},
+	{11,frame},
 };
 static TexAnim playerJumpUp[] = {
-	{2,frame},
-	{10,frame},
+	{16,frame},
+	{17,frame},
 };
 static TexAnim playerJumpDown[] = {
-	{10,frame},
-	{2,frame},
+	{17,frame},
+	{16,frame},
+};
+static TexAnim playerAttack00[] = {
+	{24,frame},
+	{25,frame},
+	{26,frame},
+	{27,frame},
+	{28,frame},
 };
 static TexAnim playerAttack01[]{
-	{4,frame},
+	{48,frame},
 };
 
 static TexAnim playerAttack02[]{
-	{12,frame},
+	{49,frame},
 };
 static TexAnim playerAttack03[]{
-	{20,frame},
+	{50,frame},
 };
 
 
 static TexAnim playerDamage[] = {
-	{6,frame},
-	{14,frame},
-	{22,frame},
+	{40,frame},
+	{41,frame},
+	{42,frame},
 };
 
 
 
 static TexAnim playerDown[] = {
-	{5,frame},
-	{13,frame},
-	{21,frame},
-	{29,frame},
+	{32,frame},
+	{33,frame},
+	{34,frame},
+	{35,frame},
 };
 
 TexAnimData player_anim_data[] = {

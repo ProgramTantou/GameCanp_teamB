3c2dbb0bdea37cc672878ac60b45631b74b32161
ProgramTantou <23051907@d.anabuki-college.net>
2024-03-21 23:34:14 +0900

    neo_juellyfits

--- Project/GameProject/Game/jellyfish_Manager.cpp
+++ Project/GameProject/Game/jellyfish_Manager.cpp
@@ -14,12 +14,12 @@ void jellyfish_Manager::Update() {
 	m_cnt--;
 	m_cut--;
 	if (m_cnt <= 0) {
-		new seaweed(CVector3D(Utility::Rand(m_scroll.x-100, m_scroll.x-100),700, (Utility::Rand(-150,150))));
+		new seaweed(CVector3D(Utility::Rand(m_scroll.x-100, m_scroll.x-100), (Utility::Rand(300, 500)), (Utility::Rand(-300,300))));
 		m_cnt = Utility::Rand(120, 240);
 	}
 	if (m_cut <= 0)
 	{
-		new seaweed(CVector3D(Utility::Rand(m_scroll.x+1920+400, m_scroll.x+1920+400), 700, (Utility::Rand(-150, 150))));
+		new seaweed(CVector3D(Utility::Rand(m_scroll.x+1920+400, m_scroll.x+1920+400), (Utility::Rand(300, 500)), (Utility::Rand(-300, 300))));
 		m_cut = Utility::Rand(120, 240);
 	}
 

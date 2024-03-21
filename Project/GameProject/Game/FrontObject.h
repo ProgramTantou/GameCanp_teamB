f5ddd7dc2f536f9f4493f14c1aad237ddb708f5d
ProgramTantou <23051907@d.anabuki-college.net>
2024-03-21 21:09:05 +0900

    Fronts

--- Project/GameProject/Game/FrontObject.h
+++ Project/GameProject/Game/FrontObject.h
@@ -5,11 +5,14 @@
 class FrontObject : public Task
 {
 public:
+	CVector2D m_pos;
+
 	CImage m_img;
 
 	FrontObject(int type);
 	void Update() override;
 	void Render() override;
+	void SetPos(const CVector2D& pos);
 
 	int front_type = 0;
 };
